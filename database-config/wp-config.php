ce179dca8c613f9728a48917c2579843f6b04aa2
musbau2013 <84200986+musbau2013@users.noreply.github.com>
2024-06-01 04:12:56 +0100

    Update wp-config.php

--- database-config/wp-config.php
+++ database-config/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'worldpress-db' );
+define( 'DB_NAME', 'wordpressdb' );
 
 /** Database username */
 define( 'DB_USER', 'admin' );
